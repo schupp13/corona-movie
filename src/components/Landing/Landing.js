b6dec282ab3c2ba4a70a42fc53fac2b59a81c754
Philip Schultz <philipschultz@pop-os.localdomain>
2020-07-20 11:06:49 -0400

    added landing page, still working on it. also added axios and material ui to project

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+    render() {
+        return (
+            <div className="landing-page page">
+                <div className="form-container">
+                    <form>
+                        <input />
+                        <input />
+                    </form>
+                </div>
+                
+            </div>
+        )
+    }
+}
+

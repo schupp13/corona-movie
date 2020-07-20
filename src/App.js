9f8c5a5593d3a14b3001a872ebe1e20ef3a3175d
Alex Maldonado <maldoalex@gmail.com>
2020-07-19 22:59:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Corona Movie Test
         </a>
       </header>
     </div>

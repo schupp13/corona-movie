726c5584c4bc9062a6ec94269cbbdd84d06cad1c
maldoalex <maldoalex@gmail.com>
2020-07-22 12:59:53 -0500

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,56 +1,29 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import "./Navbar.scss";
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  title: {
-    flexGrow: 1,
-    color: "red",
-  },
-};
-
-function NavBar(props) {
-  const classes = props;
-  return (
-    <header class="Navbar">
-      <div class="logo">{/* <Link to="/">KeepItReel</Link> */}</div>
-      <div class="Navbar-links">
-        <ul>
-          <li>Movies</li>
-          <li>TV Shows</li>
-          <li>Actors</li>
-        </ul>
-      </div>
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    </header>
-  );
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="Navbar">
+        <h1 className="Navbar-logo">KeepItReel</h1>
+        <div className="Navbar-links">
+          <ul>
+            <Link to="/movies">
+              <li>Movies</li>
+            </Link>
+            <Link to="/tvshows">
+              <li>TV Shows</li>
+            </Link>
+            <Link to="/actors">
+              <li>Actors</li>
+            </Link>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default withStyles(styles)(NavBar);
+export default Navbar;

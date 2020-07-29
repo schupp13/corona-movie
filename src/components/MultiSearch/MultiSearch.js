87ae34b5275205330fe59b79f3dccaa0b92257f6
Philip Schultz <philipschultz@pop-os.localdomain>
2020-07-28 22:49:46 -0400

    got the multi search working

--- src/components/MultiSearch/MultiSearch.js
+++ src/components/MultiSearch/MultiSearch.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import TextField from "@material-ui/core/TextField";
 import './MultiSearch.scss';
+import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
+
 
 export default class MultiSearch extends Component {
     constructor(props){
         super(props);
         this.state = {
-            results: []    
+            search: ''    
         }
     }
 
       handchange = (e)=>{
         console.log(e.target.value);
+        this.setState({
+            search: e.target.value
+        });
       }
 
     //   getSearchResults = (search) =>{
@@ -20,11 +26,14 @@ export default class MultiSearch extends Component {
 
 
     render() {
-       
+       let link = `/search/${this.state.search}`;
 
         return (
             <div className="search-input">
+                
           <TextField id="outlined-basic" label="Search for Movies, TV Shows, Actors and more..." variant="outlined" name="results" fullWidth onChange={this.handchange}/> 
+          <Button variant="contained" component={Link} to={link}>Default</Button>
+
             </div>
         )
     }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Landing from "../components/Landing/Landing";
 import HomePage from "../components/HomePage/HomePage";
 import MoviePage from "../components/MoviePage/MoviePage";
+import SearchPage from "../components/SearchPage/SearchPage";
 
 
 export default(
@@ -10,5 +11,6 @@ export default(
         <Route  exact path ="/" component={Landing}></Route>
         <Route path="/welcome" component={HomePage}></Route>
         <Route path="/movies/:id" component={MoviePage}></Route>
+        <Route path="/search/:search" component={SearchPage}></Route>
     </Switch>
 );

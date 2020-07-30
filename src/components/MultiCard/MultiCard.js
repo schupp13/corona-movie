38794207ef0331ead235f08a6eec7714a06906ab
Philip Schultz <philipschultz@pop-os.localdomain>
2020-07-30 00:54:23 -0400

    search somewhat complete

--- src/components/MultiCard/MultiCard.js
+++ src/components/MultiCard/MultiCard.js
@@ -5,22 +5,23 @@ const addDefaultSrc =(ev) =>{
   ev.target.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png';
 }
 export default function MultiCard(props) {
-  const { overview, date, title, image } = props;
-  let poster = `https://image.tmdb.org/t/p/w92/${image}`;
-  let overviewjsx = overview.length > 150 ? overview.slice(0,150) + "...": overview;
-
+  const { overview, date, title, image, id } = props;
+  let poster = image ? `https://image.tmdb.org/t/p/w92/${image}`:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png';
+  let path = props.movie ? `/movies/${props.movie.id}`: '/';
+ 
+  console.log(props);
  
   return (
-    <Link>
+    <Link to={path}>
     <div className="multicard-container">
       <div
         className="multicard-poster">
-        <img src={poster} onError={addDefaultSrc}></img>
+        <img alt={title} src={poster} onError={addDefaultSrc}></img>
       </div>
       <div className="multicard-content">
         <h4 className="title">{title}</h4>
         <p className="date">{date}</p>
-        <p className="overview">{overviewjsx}</p>
+        <p className="overview">{overview}</p>
       </div>
     </div>
     </Link>

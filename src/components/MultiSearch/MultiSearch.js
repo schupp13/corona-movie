4dd71594506feb1c011dfa2ddeba45a8706155a5
Philip Schultz <philipschultz@pop-os.localdomain>
2020-08-01 00:23:21 -0400

    search page is working well

--- src/components/MultiSearch/MultiSearch.js
+++ src/components/MultiSearch/MultiSearch.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import TextField from "@material-ui/core/TextField";
 import './MultiSearch.scss';
 import Button from '@material-ui/core/Button';
-import {Link} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
 
-export default class MultiSearch extends Component {
+ class MultiSearch extends Component {
     constructor(props){
         super(props);
         this.state = {
@@ -20,22 +23,30 @@ export default class MultiSearch extends Component {
         });
       }
 
-    //   getSearchResults = (search) =>{
-
-    //   }
-
+      submitForm = (e) => {
+        let link = `/search/${this.state.search}`;
+        e.preventDefault()
+        this.props.history.push(`/search/${this.state.search}`);  
+      }
 
     render() {
-       let link = `/search/${this.state.search}`;
 
         return (
             <div className="search-input">
-                
-          <TextField id="outlined-basic" label="Search here" variant="outlined" name="results" fullWidth onChange={this.handchange}/> 
-          <Button variant="contained" component={Link} to={link}>Default</Button>
-
+              <form  onSubmit={this.submitForm}>
+          <TextField id="outlined-basic" label="Search here" variant="outlined" fullWidth onChange={this.handchange} InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton >
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}/> 
+            </form>
             </div>
         )
     }
 }
 
+export default withRouter(MultiSearch);

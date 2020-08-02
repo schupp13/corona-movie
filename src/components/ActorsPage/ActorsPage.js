47c03e1e8ef428efae937ce3d2064712968231e6
maldoalex <maldoalex@gmail.com>
2020-08-02 11:59:43 -0500

    Actor Page

--- src/components/ActorsPage/ActorsPage.js
+++ src/components/ActorsPage/ActorsPage.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import axios from "axios";
 import ActorCard from "../ActorCard/ActorCard";
 import MoviePoster from "../MoviePoster/MoviePoster";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Pagination from "../Pagination/Pagination";
 import "./ActorsPage.scss";
 
 class ActorsPage extends Component {
@@ -13,6 +16,10 @@ class ActorsPage extends Component {
       actor: {},
       profile_path: "",
       known_for: [],
+      sortBy: "popularity.desc",
+      page: 1,
+      total_pages: 0,
+      total_results: 0,
     };
   }
 
@@ -36,21 +43,45 @@ class ActorsPage extends Component {
   };
 
   discoverActorByID = () => {
+    const { sortBy, page } = this.state;
     let actorsID = this.props.match.params.id;
     axios
       .get(
-        `https://api.themoviedb.org/3/discover/movie?api_key=12aa3499b6032630961640574aa332a9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_people=${actorsID}`
+        `https://api.themoviedb.org/3/discover/movie?api_key=12aa3499b6032630961640574aa332a9&language=en-US&sort_by=${sortBy}&include_adult=false&include_video=false&page=1&with_people=${actorsID}&page=${page}`
       )
       .then((results) => {
+        console.log(results);
         this.setState({
           known_for: results.data.results,
+          page: results.data.page,
+          total_pages: results.data.total_pages,
+          total_results: results.data.total_results,
         });
       })
       .catch();
   };
 
+  sortAction = (sort) => {
+    this.setState(
+      {
+        sortBy: sort,
+        page: 1,
+      },
+      this.discoverActorByID
+    );
+  };
+
+  paginate = (e, value) => {
+    this.setState(
+      {
+        page: value,
+      },
+      this.discoverActorByID
+    );
+  };
+
   render() {
-    const { actor, known_for } = this.state;
+    const { actor, known_for, total_pages, total_results, page } = this.state;
 
     let ActorProfile = `https://image.tmdb.org/t/p/original/${actor.profile_path}`;
 
@@ -71,26 +102,46 @@ class ActorsPage extends Component {
     return (
       <div className="actor-page">
         <div className="container">
-          <div className="left-container">
-            <div className="actor-profile">
-              <img src={ActorProfile}></img>
+          <div className="top-of-page">
+            <div className="left-container">
+              <div className="actor-profile">
+                <img src={ActorProfile}></img>
+              </div>
+              <div className="actor-personal-info">
+                <h3>Personal Info</h3>
+                <div className="actor-birthday">
+                  <h4>Birthday</h4>
+                  {actor.birthday}
+                </div>
+                <div className="actor-place-of-birth">
+                  <h4>Place of Birth</h4>
+                  {actor.place_of_birth}
+                </div>
+              </div>
             </div>
-            <div className="actor-personal-info">
-              <h3>Personal Info</h3>
-              <div className="actor-birthday">
-                <h4>Birthday</h4>
-                {actor.birthday}
-              </div>
-              <div className="actor-place-of-birth">
-                <h4>Place of Birth</h4>
-                {actor.place_of_birth}
-              </div>
+            <div className="right-container">
+              <h1 className="actor-name">{actor.name}</h1>
+              <p className="actor-bio">{actor.biography}</p>
+              <div className="actor-known-for">{DiscoverActor}</div>
             </div>
           </div>
-          <div className="right-container">
-            <h1 className="actor-name">{actor.name}</h1>
-            <p className="actor-bio">{actor.biography}</p>
-            <div className="actor-known-for">{DiscoverActor}</div>
+          <div className="movie-options-container">
+            <ButtonGroup>
+              <Button onClick={() => this.sortAction("popularity.desc")}>
+                Popular
+              </Button>
+              <Button onClick={() => this.sortAction("release_date.desc")}>
+                Filmography
+              </Button>
+            </ButtonGroup>
+            <div className="movie-options">{DiscoverActor} </div>
+            <div className="pagination">
+              <Pagination
+                page={page}
+                count={total_pages}
+                setPage={this.paginate}
+              />
+            </div>
           </div>
         </div>
       </div>
